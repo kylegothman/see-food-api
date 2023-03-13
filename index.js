0f5e89b8848c4a344cddc3f6682d7bf706f32e55
Kyle Gothman <kyle.gothman@gmail.com>
2023-03-13 13:55:29 -0500

    fixed error code

--- index.js
+++ index.js
@@ -39,15 +39,7 @@ const db = knex({
   },
 });
 
-db.connect();
 
-db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-	if (err) throw err;
-	for (let row of res.rows) {
-	  console.log(JSON.stringify(row));
-	}
-	db.end();
-  });
 
 // MIDDLEWARE
 app.use(cors(corsOptions));
