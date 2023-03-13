b9c89fd0606535d9ac7daeaea075737bcd903961
Kyle Gothman <kyle.gothman@gmail.com>
2023-03-12 21:05:22 -0500

    minor bug fix

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const image = require('./controllers/image');
 
 const db = knex({
 	client: 'pg',
-	connection: process.env.DATABASE_URL,
+	connection: {
+		connectionString : process.env.DATABASE_URL,
+		ssl: true,
+	},
   });
 
 // MIDDLEWARE
